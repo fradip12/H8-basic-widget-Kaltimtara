f9814d4d4674260ae1a38603c11aaaafcef0ac0c
fradip <fradipta.alqaiyum@koltiva.com>
2024-03-06 15:36:06 +0700

    iniitstate dispose

--- lib/pages/page_detail_resto.dart
+++ lib/pages/page_detail_resto.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:session_one/models/restaurant_models.dart';
 
-class PageRestoDetail extends StatelessWidget {
+class PageRestoDetail extends StatefulWidget {
   final RestaurantModels arguments;
   const PageRestoDetail({super.key, required this.arguments});
+
+  @override
+  State<PageRestoDetail> createState() => _PageRestoDetailState();
+}
+
+class _PageRestoDetailState extends State<PageRestoDetail> {
+  int angka = 0;
+  @override
+  void initState() {
+    super.initState();
+    print('===== Init State Page Detail =====');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('========= Dispose Page Detail ======== ');
+  }
+
+  // @override
+  // void deactivate() {
+  //   // TODO: implement deactivate
+  //   super.deactivate();
+  //   print('======= Deactivate Page Detail ======= ');
+  // }
+
+  // @override
+  // void didChangeDependencies() {
+  //   super.didChangeDependencies();
+  //   print('======== Did Change Dependenciees ======== ');
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +44,15 @@ class PageRestoDetail extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(arguments.name ?? '-'),
+        title: Text(widget.arguments.name ?? '-'),
+      ),
+      floatingActionButton: GestureDetector(
+        onTap: () => setState(() {
+          angka++;
+        }),
+        child: Icon(
+          Icons.add,
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(32.0),
@@ -21,17 +61,24 @@ class PageRestoDetail extends StatelessWidget {
             ListView.builder(
               primary: true,
               shrinkWrap: true,
-              itemCount: arguments.menus?.length,
+              itemCount: widget.arguments.menus?.length,
               itemBuilder: (_, index) {
                 return Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(arguments.menus?[index].menu ?? '-'),
-                    Text('Rp. ${arguments.menus?[index].price ?? '0'}'),
+                    Text(widget.arguments.menus?[index].menu ?? '-'),
+                    Text('Rp. ${widget.arguments.menus?[index].price ?? '0'}'),
                   ],
                 );
               },
             ),
+            Text(
+              '$angka',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            )
             // Expanded(
             //   child: Row(
             //     children: List.generate(
